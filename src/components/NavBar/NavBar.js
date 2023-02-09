fed8c4cfe39c9801f379a1f0ab014d4360c6dd7c
yashio26 <yashio200007@gmail.com>
2023-02-09 13:33:31 -0300

    Se empiezan a agregar bloques, imaginando proyecto.

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <ul>
+      <li>Productos</li>
+      <li>Sobre nosotros</li>
+    </ul>
+  );
+};
+
+export default NavBar;
---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Hola</h1>
+      <NavBar />
+    </>
+  );
+};
+
+export default Home;
