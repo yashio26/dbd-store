c881071604f578e1144ffd8af7fbdc3898897d00
yashio26 <yashio200007@gmail.com>
2023-03-01 07:16:04 -0300

    Se agrega el envio de datos para crear un usuario. Falta validar que no se repita mismo usuario al crear un nuevo registro, y el inicio de sesion.

--- src/views/Register.js
+++ src/views/Register.js
@@ -1,27 +1,77 @@
-import React from 'react';
+import { addDoc, collection } from 'firebase/firestore';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { db } from '../firebase/firebaseConfig';
+
+const initialRegister = {
+  nombre: '',
+  email: '',
+  usuario: '',
+  contraseña: '',
+};
 
 const Register = () => {
+  const [register, setRegister] = useState(initialRegister);
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setRegister({ ...register, [name]: value });
+  };
+  console.log(register);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const docRef = await addDoc(collection(db, 'usuarios'), {
+      ...register,
+    });
+    /* setContacto(docRef); */
+    setRegister(initialRegister);
+  };
+
   return (
     <div>
       <h1>Registro</h1>
-      <div style={{ backgroundColor: 'gray' }}>
+      <form onSubmit={handleSubmit} style={{ backgroundColor: 'gray' }}>
         <label>Nombre</label>
-        <input></input>
+        <input
+          required
+          type="text"
+          onChange={handleChange}
+          name="nombre"
+          value={register.nombre}
+        />
         <hr />
         <label>Correo electronico</label>
-        <input></input>
+        <input
+          required
+          type="email"
+          onChange={handleChange}
+          name="email"
+          value={register.email}
+        />
         <hr />
         <label>Nombre de usuario</label>
-        <input></input>
+        <input
+          required
+          type="text"
+          onChange={handleChange}
+          name="usuario"
+          value={register.usuario}
+        />
         <hr />
         <label>Contraseña</label>
-        <input></input>
+        <input
+          required
+          type="password"
+          onChange={handleChange}
+          name="contraseña"
+          value={register.contraseña}
+        />
         <hr />
-        <label>Confirmar contraseña</label>
-        <input></input>
+        {/* <label>Confirmar contraseña</label>
+        <input /> */}
         <button>Registrarse</button>
-      </div>
+      </form>
       <p>
         ¿Ya tienes una cuenta?{' '}
         <Link to={'/inicio-sesion'}>Inicia sesión aquí.</Link>
