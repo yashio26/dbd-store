fe9c0da809145bd7b82c37581b238dd7a586a251
yashio26 <yashio200007@gmail.com>
2023-03-08 20:08:47 -0300

    Se realiza el inicio de sesión, y su persistencia queda en sessionStorage. Falta ocultar vistas si se encuentra con la sesión iniciada.

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,19 +1,77 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { query, collection, where, getDocs } from 'firebase/firestore';
+import { db } from '../firebase/firebaseConfig';
+
+const initialLogin = { usuario: '', contraseña: '' };
 
 const Login = () => {
+  const [login, setLogin] = useState(initialLogin);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setLogin({ ...login, [name]: value });
+  };
+
+  const getUser = async () => {
+    let docs = {};
+    const q = query(
+      collection(db, 'usuarios'),
+      where('usuario', '==', login.usuario),
+      where('contraseña', '==', login.contraseña)
+    );
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      const { contraseña, usuario } = doc.data();
+      docs = { contraseña, usuario };
+    });
+    return docs;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = await getUser();
+    console.log(user);
+    if (Object.keys(user).length >= 1) {
+      console.log('setea', Object.keys(user).length);
+      sessionStorage.setItem('usuario', user.usuario);
+      navigate('/');
+    } else {
+      console.log(Object.keys(user).length);
+      alert('datos incorrectos');
+    }
+  };
+
   return (
     <div>
       <div style={{ backgroundColor: 'gray' }}>
         <h1>Inicio de sesión</h1>
-        <label>Usuario</label>
-        <input />
-        <hr />
-        <label>Contraseña</label>
-        <input />
-        <hr />
-        <button>Iniciar sesión</button>
-        <hr />
+        <form onSubmit={handleSubmit}>
+          <label>Usuario</label>
+          <input
+            required
+            type="text"
+            name="usuario"
+            value={login.usuario}
+            onChange={handleChange}
+          />
+          <hr />
+          <label>Contraseña</label>
+          <input
+            required
+            type="password"
+            name="contraseña"
+            value={login.contraseña}
+            onChange={handleChange}
+          />
+          <hr />
+          <button>Iniciar sesión</button>
+          <hr />
+        </form>
       </div>
       <Link to={'/registro'}>¿No estás registrado?</Link>
     </div>
