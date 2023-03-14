e489f8530d89badf27c9816a36295bd3fe4d29e8
yashio26 <yashio200007@gmail.com>
2023-03-13 21:27:13 -0300

    Se suma un nuevo state, este controla el precio total de los productos.

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -5,7 +5,23 @@ const CartContext = createContext();
 export const CartProvider = ({ children }) => {
   const [carrito, setCarrito] = useState([]);
 
-  const data = { carrito, setCarrito };
+  const [precioTotal, setPrecioTotal] = useState(0);
+
+  const calculoPrecioTotal = () => {
+    let precio = 0;
+    carrito.map((el) => {
+      console.log(el.total);
+      return (precio = precio + el.total);
+    });
+    setPrecioTotal(precio);
+  };
+
+  const data = {
+    carrito,
+    setCarrito,
+    precioTotal,
+    calculoPrecioTotal,
+  };
 
   return <CartContext.Provider value={data}>{children}</CartContext.Provider>;
 };
--- src/views/Carrito.js
+++ src/views/Carrito.js
@@ -1,15 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import CartItem from '../components/CartItem';
 import CartContext from '../context/CartContext';
 
 const Carrito = () => {
-  const { carrito } = useContext(CartContext);
+  const { carrito, precioTotal, calculoPrecioTotal } = useContext(CartContext);
+
+  useEffect(() => {
+    calculoPrecioTotal();
+  }, [calculoPrecioTotal]);
 
   return (
     <div>
       <h1>Carrito</h1>
       {carrito.length > 0 ? (
-        carrito.map((carro) => <CartItem key={carro.id} info={carro} />)
+        <>
+          {carrito.map((carro) => (
+            <CartItem key={carro.id} info={carro} />
+          ))}
+          <h2>{precioTotal}</h2>
+        </>
       ) : (
         <h1>No hay ningun producto</h1>
       )}
