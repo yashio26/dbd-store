399b6105e7a8e36982294f5776a9e8873b744cb0
yashio26 <yashio200007@gmail.com>
2023-03-15 18:15:42 -0300

    Se agrega Link al producto que está en carrito.

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CartItem = ({ info }) => {
   return (
     <div>
       <h1>{info.producto}</h1>
-      <img width={'50px'} height={'50px'} src={info.img} alt="a" />
+      <Link to={`/producto/${info.id}`}>
+        <img width={'50px'} height={'50px'} src={info.img} alt="a" />
+      </Link>
       <p>
         {info.cantidad} - {info.precio} - {info.total}
       </p>
