44f7daee2ae369d8c6ca8cb51e605bc6903d5fef
yashio26 <yashio200007@gmail.com>
2023-03-23 00:22:04 -0300

    Se agrega la lógica y el formulario para poder editar un producto.

--- src/views/Admin.js
+++ src/views/Admin.js
@@ -6,6 +6,7 @@ import {
   documentId,
   getDocs,
   query,
+  updateDoc,
   where,
 } from 'firebase/firestore';
 import React, { useState } from 'react';
@@ -24,12 +25,26 @@ const initialDelete = {
   id: '',
 };
 
+const initialUpdate = {
+  id: '',
+  categoria: '',
+  descripcion: '',
+  img: '',
+  precio: 0,
+  producto: '',
+  stock: 0,
+};
+
 const Admin = () => {
   const [newProduct, setNewProduct] = useState(initialProduct);
 
-  const [delProduct, setDelProduct] = useState(initialDelete);
+  const [deleteChange, setDeleteChange] = useState(initialDelete);
 
-  const [product, setProduct] = useState(null);
+  const [deleteProduct, setDeleteProduct] = useState(null);
+
+  const [updateChange, setUpdateChange] = useState(initialUpdate);
+
+  const [productToEdit, setProductToEdit] = useState(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -52,7 +67,7 @@ const Admin = () => {
 
   const handleDelChange = (e) => {
     const { name, value } = e.target;
-    setDelProduct({ ...delProduct, [name]: value });
+    setDeleteChange({ ...deleteChange, [name]: value });
   };
 
   const handleDelSubmit = async (e) => {
@@ -62,7 +77,7 @@ const Admin = () => {
       const querySnapshot = await getDocs(
         query(
           collection(db, 'tienda'),
-          where(documentId(), '==', delProduct.id)
+          where(documentId(), '==', deleteChange.id)
         )
       );
       querySnapshot.forEach((doc) => {
@@ -72,11 +87,61 @@ const Admin = () => {
     };
     const productExist = await traerProducto();
     if (!!productExist) {
-      await deleteDoc(doc(db, 'tienda', delProduct.id));
-      setDelProduct(initialDelete);
-      setProduct(`Se eliminó ${productExist.producto}.`);
+      await deleteDoc(doc(db, 'tienda', deleteChange.id));
+      setDeleteChange(initialDelete);
+      setDeleteProduct(`Se eliminó ${productExist.producto}.`);
     } else {
-      setProduct(`No se encontró el producto.`);
+      console.log(`No se encontró el producto.`);
+    }
+  };
+
+  const handleUpdChange = (e) => {
+    const { name, value } = e.target;
+    setUpdateChange({ ...updateChange, [name]: value });
+    console.log(updateChange);
+  };
+
+  const handleUpdSubmit = async (e) => {
+    e.preventDefault();
+    if (productToEdit) {
+      console.log(productToEdit, updateChange);
+      const { producto, categoria, descripcion, img, precio, stock } =
+        updateChange;
+      await updateDoc(doc(db, 'tienda', updateChange.id), {
+        producto,
+        categoria,
+        descripcion,
+        img,
+        precio,
+        stock,
+      });
+      setUpdateChange(initialUpdate);
+      setProductToEdit(null);
+    } else {
+      console.log(productToEdit);
+      const traerProducto = async () => {
+        let docs = null;
+        const querySnapshot = await getDocs(
+          query(
+            collection(db, 'tienda'),
+            where(documentId(), '==', updateChange.id)
+          )
+        );
+        querySnapshot.forEach((doc) => {
+          docs = { ...doc.data(), id: doc.id };
+        });
+        return docs;
+      };
+      const productExist = await traerProducto();
+      if (!!productExist) {
+        setProductToEdit(true);
+        setUpdateChange(productExist);
+        /* await updateDoc(doc(db, 'tienda', updateChange.id), {
+        stock: 0,
+      }); */
+      } else {
+        console.log(`No se encontró el producto.`);
+      }
     }
   };
 
@@ -84,75 +149,156 @@ const Admin = () => {
     <div>
       <h1>Panel de administrador</h1>
       <h2>Agregar producto</h2>
-      <form onSubmit={handleSubmit}>
-        <label>Inserte el nombre del producto.</label>
-        <input
-          required
-          type="text"
-          name="producto"
-          value={newProduct.producto}
-          onChange={handleChange}
-        />
-        <hr />
-        <label>Inserte la categoria del producto.</label>
-        <input
-          required
-          type="text"
-          name="categoria"
-          value={newProduct.categoria}
-          onChange={handleChange}
-        />
-        <hr />
-        <label>Inserte precio del producto.</label>
-        <input
-          required
-          type="number"
-          name="precio"
-          value={newProduct.precio}
-          onChange={handleChange}
-        />
-        <hr />
-        <label>Inserte el stock del producto.</label>
-        <input
-          required
-          type="number"
-          name="stock"
-          value={newProduct.stock}
-          onChange={handleChange}
-        />
-        <hr />
-        <label>Inserte la descripcion del producto.</label>
-        <input
-          required
-          type="text"
-          name="descripcion"
-          value={newProduct.descripcion}
-          onChange={handleChange}
-        />
-        <hr />
-        <label>Inserte link de la imagen del producto.</label>
-        <input
-          required
-          type="url"
-          name="img"
-          value={newProduct.img}
-          onChange={handleChange}
-        />
-        <button>Enviar</button>
-      </form>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>Inserte el nombre del producto.</label>
+          <input
+            required
+            type="text"
+            name="producto"
+            value={newProduct.producto}
+            onChange={handleChange}
+          />
+          <hr />
+          <label>Inserte la categoria del producto.</label>
+          <input
+            required
+            type="text"
+            name="categoria"
+            value={newProduct.categoria}
+            onChange={handleChange}
+          />
+          <hr />
+          <label>Inserte precio del producto.</label>
+          <input
+            required
+            type="number"
+            name="precio"
+            value={newProduct.precio}
+            onChange={handleChange}
+          />
+          <hr />
+          <label>Inserte el stock del producto.</label>
+          <input
+            required
+            type="number"
+            name="stock"
+            value={newProduct.stock}
+            onChange={handleChange}
+          />
+          <hr />
+          <label>Inserte la descripcion del producto.</label>
+          <input
+            required
+            type="text"
+            name="descripcion"
+            value={newProduct.descripcion}
+            onChange={handleChange}
+          />
+          <hr />
+          <label>Inserte link de la imagen del producto.</label>
+          <input
+            required
+            type="url"
+            name="img"
+            value={newProduct.img}
+            onChange={handleChange}
+          />
+          <button>Enviar</button>
+        </form>
+      </div>
       <h2>Eliminar producto</h2>
       <div>
         <form onSubmit={handleDelSubmit}>
+          <label>Inserte ID del producto.</label>
           <input
             required
             type="text"
             name="id"
-            value={delProduct.id}
+            value={deleteChange.id}
             onChange={handleDelChange}
           />
           <button>Eliminar producto</button>
         </form>
-        {!!product && <p>{product}</p>}
+        {!!deleteProduct && <p>{deleteProduct}</p>}
+      </div>
+      <h2>Actualizar producto</h2>
+      <div>
+        <form
+          onSubmit={handleUpdSubmit}
+          className={productToEdit && 'disabled'}
+        >
+          <label>Inserte ID del producto.</label>
+          <input
+            required
+            type="text"
+            name="id"
+            value={updateChange.id}
+            onChange={handleUpdChange}
+          />
+          {productToEdit && (
+            <>
+              <hr />
+              <label>Producto</label>
+              <input
+                required
+                type="text"
+                name="producto"
+                value={updateChange.producto}
+                onChange={handleUpdChange}
+              />
+              <hr />
+              <label>Categoria</label>
+              <input
+                required
+                type="text"
+                name="categoria"
+                value={updateChange.categoria}
+                onChange={handleUpdChange}
+              />
+              <hr />
+              <label>Descripcion</label>
+              <input
+                required
+                type="text"
+                name="descripcion"
+                value={updateChange.descripcion}
+                onChange={handleUpdChange}
+              />
+              <hr />
+              <label>Imagen</label>
+              <input
+                required
+                type="url"
+                name="img"
+                value={updateChange.img}
+                onChange={handleUpdChange}
+              />
+              <hr />
+              <label>Precio</label>
+              <input
+                required
+                type="number"
+                name="precio"
+                value={updateChange.precio}
+                onChange={handleUpdChange}
+              />
+              <hr />
+              <label>Stock</label>
+              <input
+                required
+                type="number"
+                name="stock"
+                value={updateChange.stock}
+                onChange={handleUpdChange}
+              />
+              <hr />
+            </>
+          )}
+          <button>
+            {productToEdit ? 'Actualizar producto' : 'Buscar producto'}
+          </button>
+        </form>
       </div>
     </div>
   );
